fb5bb7f648f246631adf78c70e30986f5d2067c3
annisa <annisarahmaa224@students.amikom.ac.id>
2025-10-22 10:24:58 +0700

    add home activity

---
+++ app/src/main/java/com/example/pizzaapp3387/HomeActivity.kt
@@ -0,0 +1,36 @@
+package com.example.pizzaapp3387
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class HomeActivity : AppCompatActivity() {
+    //function replace fragment
+    private fun  replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTrx = fragmentManager.beginTransaction()
+        fragmentTrx.replace(R.id.fragmentContainerView, fragment)
+        fragmentTrx.commit()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //instance
+        val txtAccount: TextView =findViewById(R.id.textViewMenuAcount)
+        val txtMenu: TextView =findViewById(R.id.textViewMenu)
+        val txtTransition: TextView=findViewById(R.id.textViewTransaction)
+        val txtReport: TextView=findViewById(R.id.textViewReport)
+    }
+}
